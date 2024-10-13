f205268f71fa6f21cc469d6ab9ef96e56b8568b2
VENKATESWARLU DURGAM <104641384+dvenkateswarlu@users.noreply.github.com>
2024-10-13 14:46:27 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Program2 {
+    public static void main(String[] args) {
+        List<String> s = List.of("babu", "Neon", "babgi", "bagi", "roja");
+
+        s.stream()
+         .filter(str -> str.charAt(0) == 'b')  
+         .map(s1 -> s1.toUpperCase())
+         .forEach(i -> System.out.println(i));
+
+        System.out.println(s);
+    }
+}
