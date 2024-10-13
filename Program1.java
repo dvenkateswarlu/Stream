f82b2c98254c76852785b07dc974180fe2fa91f3
VENKATESWARLU DURGAM <104641384+dvenkateswarlu@users.noreply.github.com>
2024-10-13 14:43:24 +0530

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		List<String> s = List.of("babu","Neon","babgi","bagi","roja");
+		
+		s.stream()
+		.filter(str -> str.charAt(0) == 'b')
+		.map(s1 -> s1.toUpperCase())
+		.sorted((s1,s2) -> s2.compareTo(s1))
+		.forEach(i -> System.out.println(i));
+	
+// 	s.stream()
+//          .filter(str -> str.charAt(0) == 'b')  // corrected charAt()
+//          .map(s1 -> s1.toUpperCase())
+//          .forEach(i -> System.out.println(i));
+         
+		System.out.println(s);
+	}
+}
